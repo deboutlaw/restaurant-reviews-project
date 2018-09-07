f5f4ade0635dfdd3aa6faa50cbf40d2fc78450f6
deborah outlaw <deborahaoutlaw@gmail.com>
2018-09-07 13:43:00 -0400

    corrections

--- js/sw.js
+++ js/sw.js
@@ -7,26 +7,26 @@ self.addEventListener('install', e=>{
     .then(cache =>{
       console.log('service worker: caching files');
       return cache.addAll([
-          '/skeleton',
+
           '/js/main.js',
           '/js/dbhelper.js',
           '/js/restaurant_info.js',
           '/css/styles.css',
           '/data/restaurants.json',
-          '/imgs/1.jpg',
-          '/imgs/2.jpg',
-          '/imgs/3.jpg',
-          '/imgs/4.jpg',
-          '/imgs/5.jpg',
-          '/imgs/6.jpg',
-          '/imgs/7.jpg',
-          '/imgs/8.jpg',
-          '/imgs/9.jpg',
-          '/imgs/10.jpg',
+          '/img/1.jpg',
+          '/img/2.jpg',
+          '/img/3.jpg',
+          '/img/4.jpg',
+          '/img/5.jpg',
+          '/img/6.jpg',
+          '/img/7.jpg',
+          '/img/8.jpg',
+          '/img/9.jpg',
+          '/img/10.jpg',
           '/index.html',
           '/restaurant.html'
 
-        ])
+        ]);
     })
   );
 });
@@ -53,19 +53,14 @@ self.addEventListener('fetch', e => {
   console.log('service worker: fetching');
   const requestUrl = new URL(e.request.url);
     if (requestUrl.origin === location.origin) {
-      if (requestUrl.pathname === '/') {
-        e.respondWith(caches.match('/skeleton'));
-        return;
-      }
+      e.respondWith(
+        caches.match(e.request)
+        .then(response=> {
+          return response || fetch(e.request);
+        }));
     }
+  });
 
-  e.respondWith(
-    caches.match(e.request)
-    .then(response=> {
-      return response || fetch(e.request);
-    })
-  );
-});
 
 self.addEventListener('message', function(event) {
   if (event.data.action === 'skipWaiting') {
